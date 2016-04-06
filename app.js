99c4f6bca55df118093ea6fd2debe78743452aff
oleg <kaizersose@gmail.com>
2016-04-07 00:05:57 +0300

    added bye api

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get('/', function (req, res) {
     res.send('Hello World! :)');
 });
 
+app.get('/bye', function (req, res) {
+    res.send('bye bye');
+});
+
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
     console.log('just a test :)')
